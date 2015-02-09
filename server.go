b968c4329cef1a9a3cb40fcf9b9d520aaee6e078
Florian Fankhauser <flo@lingu.at>
2015-02-09 23:02:40 +0100

    Initial commit of golang server

---
+++ server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "os"
+  "io"
+)
+
+func main() {
+  http.Handle("/", http.FileServer(http.Dir("./public")))
+
+  http.HandleFunc("/comments.json", func(w http.ResponseWriter, r *http.Request) {
+    file, err := os.Open("_comments.json")
+    if err != nil {
+      panic(err)
+    }
+    w.Header().Set("Content-Type", "application/json")
+    io.Copy(w, file)
+  })
+
+  fmt.Println("Starte Server")
+  http.ListenAndServe(":3000", nil)
+}
